b8a3d74194de7ad996d0dbc79f28477bde5477f3
Kaarlo Räihä <mcraiha@users.noreply.github.com>
2024-11-17 20:33:10 +0200

    Add another test case for Read WAV
    Because original wav has some additional info

--- tests/WavHelperTests.cs
+++ tests/WavHelperTests.cs
@@ -10,10 +10,34 @@ public class WavHelperTests
 	}
 
 	[Test, Description("Read data from mono 48 000 Hz WAV audio file")]
-	public void ReadWavTest()
+	public void ReadWavMono1Test()
 	{
 		// Arrange
 		string input_WAV_Filename = "samples/car_trunk_close.qoa.wav";
+		uint expectedSamplerate = 48_000;
+		uint expectedSampleCount = 35530;
+		uint expectedChannelsCount = 1;
+
+		// Act
+		short[] sampledata = Array.Empty<short>();
+		(uint channels, uint samplerate, uint samples) specs;
+		using (FileStream inputStream = File.OpenRead(input_WAV_Filename))
+		{
+			sampledata = WavHelper.ReadWav(inputStream, out specs);
+		}
+
+		// Assert
+		Assert.That(expectedSampleCount, Is.EqualTo(sampledata.Length));
+		Assert.That(expectedSamplerate, Is.EqualTo(specs.samplerate));
+		Assert.That(expectedSampleCount, Is.EqualTo(specs.samples));
+		Assert.That(expectedChannelsCount, Is.EqualTo(specs.channels));
+	}
+
+	[Test, Description("Read data from mono 48 000 Hz WAV audio file")]
+	public void ReadWavMono2Test()
+	{
+		// Arrange
+		string input_WAV_Filename = "samples/car_trunk_close.wav";
 		uint expectedSamplerate = 48_000;
 		uint expectedSampleCount = 35530;
 		uint expectedChannelsCount = 1;

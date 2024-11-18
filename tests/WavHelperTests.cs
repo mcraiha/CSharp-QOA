530c470579e241d0d744541d893c9b00cfbc8316
Kaarlo Räihä <mcraiha@users.noreply.github.com>
2024-11-18 20:28:51 +0200

    Add first stereo sample WAV
    And include a test case for it

--- tests/WavHelperTests.cs
+++ tests/WavHelperTests.cs
@@ -56,4 +56,29 @@ public class WavHelperTests
 		Assert.That(expectedSampleCount, Is.EqualTo(specs.samples));
 		Assert.That(expectedChannelsCount, Is.EqualTo(specs.channels));
 	}
+
+	[Test, Description("Read data from stereo 48 000 Hz WAV audio file")]
+	public void ReadWavStereo1Test()
+	{
+		// Arrange
+		string input_WAV_Filename = "samples/sting_xp_level_up_orch_01.wav";
+		uint expectedSamplerate = 48_000;
+		uint expectedTotalSampleCount = 191_942;
+		uint expectedSamplesPerChannelCount = expectedTotalSampleCount / 2;
+		uint expectedChannelsCount = 2;
+
+		// Act
+		short[] sampledata = Array.Empty<short>();
+		(uint channels, uint samplerate, uint samples) specs;
+		using (FileStream inputStream = File.OpenRead(input_WAV_Filename))
+		{
+			sampledata = WavHelper.ReadWav(inputStream, out specs);
+		}
+
+		// Assert
+		Assert.That(expectedTotalSampleCount, Is.EqualTo(sampledata.Length));
+		Assert.That(expectedSamplerate, Is.EqualTo(specs.samplerate));
+		Assert.That(expectedSamplesPerChannelCount, Is.EqualTo(specs.samples));
+		Assert.That(expectedChannelsCount, Is.EqualTo(specs.channels));
+	}
 }
